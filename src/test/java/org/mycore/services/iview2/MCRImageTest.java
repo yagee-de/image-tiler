ab17d47dff614c7854c26642366bfd39edd8cedd
Huu Chi Vu <chi.vuhuu@gmail.com>
2009-12-03 20:38:25 +0000

    adding JUnit test
    git-svn-id: https://server.mycore.de/svn/mycore-iview2-addons/image-tiler/trunk@16729 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ src/test/java/org/mycore/services/iview2/MCRImageTest.java
@@ -0,0 +1,27 @@
+package org.mycore.services.iview2;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.apache.commons.io.FilenameUtils;
+
+import junit.framework.TestCase;
+
+public class MCRImageTest extends TestCase {
+    private HashMap<String, String> pics = new HashMap<String, String>();
+    @Override
+    protected void setUp() throws Exception {
+        pics.put("small", "src/test/resources/Bay_of_Noboto.jpg");
+        super.setUp();
+    }
+    public void testTiling() throws Exception {
+        String filePath = pics.get("large");
+        File file =new File(filePath);
+        MCRImage image = new MCRImage(file , "derivateID", "imagePath/" + FilenameUtils.getName(filePath));
+        File tiledir = new File("target/tileDir");
+        image.setTileDir(tiledir);
+        image.tile();
+        
+        assertTrue(tiledir.exists());
+    }
+}
